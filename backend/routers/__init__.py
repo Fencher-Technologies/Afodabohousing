4281335f4724fbb6008290bdf3759a4ae3f6bc3b
YonahAyesiga <yonahayesiga241@gmail.com>
2026-07-08 17:15:28 +0300

    Fix import order in routers/__init__.py to satisfy Ruff linter

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -4,13 +4,13 @@ from .leases import router as leases_router
 from .maintenance_requests import router as maintenance_requests_router
 from .managers import router as managers_router
 from .messages import router as messages_router
+from .notifications import router as notifications_router
 from .payments import router as payments_router
 from .properties import router as properties_router
 from .rental_units import router as rental_units_router
 from .tenants import router as tenants_router
 from .uploads import router as uploads_router
 from .webhooks import router as webhooks_router
-from .notifications import router as notifications_router
 
 __all__ = [
     "auth_router",
